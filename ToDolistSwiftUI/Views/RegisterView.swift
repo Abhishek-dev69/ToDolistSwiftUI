62f7b3e02c81649dd3bd49191f56532ad410e0e8
Abhishek <abhishek12439525@gmail.com>
2026-01-22 03:57:32 +0530

    made the register view with error handling

--- ToDolistSwiftUI/Views/RegisterView.swift
+++ ToDolistSwiftUI/Views/RegisterView.swift
@@ -9,37 +9,44 @@ import SwiftUI
 
 struct RegisterView: View {
     @StateObject var viewModel = RegisterViewViewModel()
+
     var body: some View {
-        VStack{
-            HeaderView(title:"Register",
-            subtitle: "Start organizing todos",
-                       angle: -15,
-                       background: .orange)
-            Form{
+        VStack {
+            HeaderView(
+                title: "Register",
+                subtitle: "Start organizing todos",
+                angle: -15,
+                background: .orange
+            )
+
+            if !viewModel.errorMessage.isEmpty {
+                Text(viewModel.errorMessage)
+                    .foregroundColor(.red)
+                    .padding(.horizontal)
+            }
+
+            Form {
                 TextField("Full Name", text: $viewModel.name)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
                     .autocorrectionDisabled()
+
                 TextField("Email Address", text: $viewModel.email)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
                     .autocapitalization(.none)
                     .autocorrectionDisabled()
+
                 SecureField("Password", text: $viewModel.password)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                TLButton(
-                    title:"Create Account",
-                    background: .green
-                ){
-                    //Attempt registration
+
+                TLButton(title: "Create Account", background: .green) {
                     viewModel.register()
-            }
+                }
                 .padding()
             }
             .offset(y: -50)
-            
+
             Spacer()
         }
     }
 }
+
 
 #Preview {
     RegisterView()
